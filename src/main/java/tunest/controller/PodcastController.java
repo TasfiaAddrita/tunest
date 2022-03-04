7ef6683b9fb8a28ddb9f060925965b9873f66d24
TasfiaAddrita <tasfia.addrita@gmail.com>
2022-03-04 13:22:04 -0500

    feat(api): create endpoint to get a podcast by id

--- src/main/java/tunest/controller/PodcastController.java
+++ src/main/java/tunest/controller/PodcastController.java
@@ -1,6 +1,7 @@
 package tunest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tunest.model.Podcast;
@@ -21,5 +22,11 @@ public class PodcastController {
     public List<Podcast> getAllPodcasts() {
         List<Podcast> podcasts = this.podcastRepository.findAll();
         return podcasts;
+    }
+
+    @GetMapping("/podcast/{id}")
+    public Podcast getPodcastById(@PathVariable("id") String id) {
+        Podcast podcast = this.podcastRepository.findById(id).orElseThrow();
+        return podcast;
     }
 }
