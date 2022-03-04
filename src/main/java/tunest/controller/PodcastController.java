72b5aa64d81c43dc3d6ca746b82e29c88f424313
TasfiaAddrita <tasfia.addrita@gmail.com>
2022-03-04 13:31:50 -0500

    feat(api): create endpoint to insert a new podcast

--- src/main/java/tunest/controller/PodcastController.java
+++ src/main/java/tunest/controller/PodcastController.java
@@ -1,9 +1,6 @@
 package tunest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tunest.model.Podcast;
 import tunest.repository.PodcastRepository;
 
@@ -28,5 +25,10 @@ public class PodcastController {
     public Podcast getPodcastById(@PathVariable("id") String id) {
         Podcast podcast = this.podcastRepository.findById(id).orElseThrow();
         return podcast;
+    }
+
+    @PostMapping("/podcast")
+    public void createPodcast(@RequestBody Podcast podcast) {
+        this.podcastRepository.insert(podcast);
     }
 }
